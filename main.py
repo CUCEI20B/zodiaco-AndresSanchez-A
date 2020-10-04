4e746b46b045635fb26a859fb8becb0849021266
Andres Sanchez <andres.sanchez5922@alumnos.udg.mx>
2020-10-04 01:03:16 -0500

    prueba 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hola Mundo")
+print("Simbolo del Zodiaco")
+mes = int(input("Numero de mes de nacimiento: "))
+dia = int(input("Numero de dia de nacimiento: "))
+if mes == 12 or mes == 1: 
+    if dia >= 22 and dia <= 31:
+        print("Tu simbolo es Capricornio")
+    if dia >= 1 and dia <= 20:
+        print("Tu simbolo es Capricornio")
+else:
+    print("Error en lo datos ingresados")
